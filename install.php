4f8537da259910f1c8c21ad98065386e2178102f
梁松华 <1411008034@qq.com>
2016-06-10 11:48:15 +0800

    Update install.php
    更正配置项

--- install.php
+++ install.php
@@ -1,8 +1,11 @@
 #nginx配置
 ./configure --prefix=/usr/local/nginx \
   --with-pcre=/home/pidan/tar/pcre-8.37 \
-  --with-openssl=/usr/local/openssl \
+   --with-openssl=/home/pidan/tar/openssl-1.0.0e \
   --pid-path=/usr/local/nginx/logs/nginx.pid  \
+#--with-openssl=PATH 选项指定的 PATH 是 OpenSSL 的源码目录，而不是 OpenSSL 
+的安装目录。安装目录应使用 --with-cc-opt 和 --with-ld-opt 选项
+
 
 #php配置
 ./configure --prefix=/usr/local/php \
